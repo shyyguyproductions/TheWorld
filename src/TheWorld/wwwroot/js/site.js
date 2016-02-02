64854e76a4c618dd75fb3fdc79f85e09b67e4e0f
Paul Shyy <shyyguygames@gmail.com>
2016-02-01 20:20:44 -0800

    Initial Commit.  Project at end of javascript module.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿//site.js
+(function () {
+
+    // uses css names to find elements ex. #username
+    //var ele = $("#username");
+    //ele.text("Shyy Guy");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+    
+
+})();
